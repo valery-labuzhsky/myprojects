4f4de026b55eb24acdf9d7a37fdb1b2050fdc610
valery-labuzhsky <ynicorn@gmail.com>
2020-05-03 16:42:38 +0300

    Profile renamed

--- chess/chess.sh
+++ chess/chess.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cd /home/ptasha/projects/myprojects/chess
-/usr/lib/jvm/java-11-openjdk-amd64/bin/java -classpath /home/ptasha/projects/myprojects/chess:/home/ptasha/projects/myprojects/chess/target/classes ChessEngineClient
+cd /home/unicorn/projects/myprojects/chess
+/usr/lib/jvm/java-11-openjdk-amd64/bin/java -classpath /home/unicorn/projects/myprojects/chess:/home/unicorn/projects/myprojects/chess/target/classes ChessEngineClient
 exit 0
